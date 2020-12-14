839a0ccd99bcbb6acc4debd320ae302928569185
Jesper Müllern <mullern23@hotmail.com>
2020-12-14 15:36:04 +0100

    demo

---
+++ Lektion 1/4. variables/script.js
@@ -0,0 +1,25 @@
+/* 
+
+    VARIABLES
+
+    en container/box för att lagra information i. Heap/Stack
+    
+    JS skriver man med camelCase
+    JS:  camelCase  var firstName
+    C#:  Pascal     var FirstName
+
+
+*/
+
+
+// value        // Gör absolut inte så här
+// var value    // undvik att göra så här
+// let value | kan ändras men ej deklareras igen  // gör så här för ett dynamiskt värde (R/W)
+// const value | kan ej skrivas över  // gör så här för ett statiskt värde (R)
+
+let value
+
+value = 36
+
+
+
